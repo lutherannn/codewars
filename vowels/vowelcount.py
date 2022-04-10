1bccc3cf7a03574fb39665e032af0ec31af7238d
Michael <michael.eskelin@gmail.com>
2022-04-10 13:47:59 -0400

    Python vowel count

---
+++ vowels/vowelcount.py
@@ -0,0 +1,9 @@
+
+
+def vowelCount(word):
+  vowels = ["a", "e", "i", "o", "u"]
+  vowelNum = 0
+  for vowels in word:
+    vowelNum += 1
+  print("Number of vowels in given string" + ": " + str(vowelNum))
+vowelCount("my very execellent mother just cooked us noodles")
